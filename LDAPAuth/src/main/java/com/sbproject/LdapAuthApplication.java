5f3805e6183af742fa02467adadeda91ca899207
Shajith K <shajit.krishnan@gmail.com>
2018-05-03 14:26:27 +1000

    Adding LDAP auth project.

---
+++ LDAPAuth/src/main/java/com/sbproject/LdapAuthApplication.java
@@ -0,0 +1,12 @@
+package com.sbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapAuthApplication.class, args);
+	}
+}
